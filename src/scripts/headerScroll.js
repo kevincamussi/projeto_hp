523e30442cdd648bdde8865ee43c87e5eb2a48ab
kevincamussi <kevincamussi@gmail.com>
2024-02-12 12:25:15 -0300

    adicionando scrollspy

--- src/scripts/headerScroll.js
+++ src/scripts/headerScroll.js
@@ -1,7 +1,15 @@
 $(document).ready(function(){
-    $(window).on('scroll', function() {
-        const pageHeader =  $('.head');
-        const height = 32;
+    const navTab = $('.head-menu__link');
+    const pageHeader =  $('.head');
+    const height = 32;
+
+    navTab.click(function(){
+        navTab.removeClass('active');
+        $(this).addClass('active');
+    })
+
+
+    $(document).on('scroll', function() {
 
         function addBgColor () {
             pageHeader.addClass('bg-black');
@@ -9,8 +17,25 @@ $(document).ready(function(){
 
         function removeBgColor () {
             pageHeader.removeClass('bg-black');
+            pageHeader.css({'transition': '0.8s'});
         }
 
         $(this).scrollTop() > height ? addBgColor() : removeBgColor();
+
+
+        navTab.each(function(){
+
+            let container = $(this).attr('href');
+            let containerOffset = $(container).offset().top;
+            let containerHeight = $(container).outerHeight();
+            let containerBottom = containerOffset + containerHeight;
+            let scrollPosition = $(document).scrollTop();
+    
+            if(scrollPosition < containerBottom - 20 && scrollPosition >= containerOffset - 20){
+                $(this).addClass('active');
+            } else{
+                $(this).removeClass('active');
+            }
+        });
     })
 })
